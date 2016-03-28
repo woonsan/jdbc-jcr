0f7673753dec9cd7d2cd0608772457a5124aa44f
woonsan <woonsan@apache.org>
2016-03-28 04:06:40 -0400

    more unit tests in JcrJdbcResultSetMetaDataTest

--- src/test/java/com/github/woonsan/jdbc/jcr/impl/JcrJdbcResultSetMetaDataTest.java
+++ src/test/java/com/github/woonsan/jdbc/jcr/impl/JcrJdbcResultSetMetaDataTest.java
@@ -21,6 +21,7 @@ package com.github.woonsan.jdbc.jcr.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.sql.ResultSetMetaData;
 
@@ -43,6 +44,106 @@ public class JcrJdbcResultSetMetaDataTest {
             assertTrue(metaData.isReadOnly(i));
             assertFalse(metaData.isWritable(i));
             assertFalse(metaData.isDefinitelyWritable(i));
+        }
+    }
+
+    @Test
+    public void testUnsupportedOperations() throws Exception {
+        String [] columnNames = { "col1", "col2", "col3" };
+        ResultSetMetaData metaData = new JcrJdbcResultSetMetaData(columnNames);
+
+        try {
+            metaData.isWrapperFor(null);
+            fail();
+        } catch (UnsupportedOperationException ignore) {}
+
+        try {
+            metaData.unwrap(null);
+            fail();
+        } catch (UnsupportedOperationException ignore) {}
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.isCurrency(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.isNullable(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.isSigned(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getColumnDisplaySize(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getSchemaName(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getPrecision(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getScale(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getTableName(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getCatalogName(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getColumnType(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getColumnTypeName(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
+        }
+
+        for (int i = 1; i <= columnNames.length; i++) {
+            try {
+                metaData.getColumnClassName(i);
+                fail();
+            } catch (UnsupportedOperationException ignore) {}
         }
     }
 
